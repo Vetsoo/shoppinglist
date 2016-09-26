8f93ba3972620abea149ecd622b65db74a3e10b6
Jente Vets <jve@emakina.com>
2016-09-26 17:49:26 +0200

    First commit with 2 models added (product and shoppinglist)

---
+++ ShoppingList/ShoppingList/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingList.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public virtual ICollection<ShoppingList> ShoppingLists { get; set; }
+    }
+}
---
+++ ShoppingList/ShoppingList/Models/ShoppingList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingList.Models
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
